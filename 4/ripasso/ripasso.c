3de171eab71930ec54946b37765875ca082b03a0
leonardo <leonardo.osti@iisviolamarchesini.edu.it>
2024-01-17 13:15:17 -0800

    Ripasso finito

--- 4/ripasso/ripasso.c
+++ 4/ripasso/ripasso.c
@@ -3,16 +3,42 @@
 #include <unistd.h>
 
 #define array_length 10000
-#define BUFFER_DIM 4
 
 int figlioA, figlioB;
-unsigned char buffer[BUFFER_DIM];
 int main(int argc, char *argv[])
 {
+    int arrayRandom[array_length];
+    FILE *numeri;
+    numeri = fopen("numeriRandom.txt", "w");
+    if (numeri == NULL)
+    {
+        printf("Errore nell'apertura del file\n");
+        exit(1);
+    }
+    for (int i = 0; i < array_length; i++)
+    {
+        arrayRandom[i] = rand() % 500 + 1; // Generatore casuale di numeri da 1 a 500
+        fprintf(numeri, "%d %s %d\n", i, ":", arrayRandom[i]);
+    }
+    fclose(numeri); // chiuso lo stream
+    if (argc != 2)
+    {
+        printf("Argomenti insufficenti\n");
+        exit(1);
+    }
+    int n = atoi(argv[1]); // converto in intero il numero inserito dall'utente
     figlioA = fork();
     if (figlioA == 0)
     {
         // Figlio A
+        printf("Ricerca processo figlio A\n");
+        for (int i = 2000; i < 6000; i++)
+        {
+            if (n == arrayRandom[i])
+            {
+                printf("PID: %d, %d : %d\n", getpid(), i, arrayRandom[i]);
+            }
+        }
     }
     else
     {
@@ -20,27 +46,26 @@ int main(int argc, char *argv[])
         if (figlioB == 0)
         {
             // Figlio B
+            printf("Ricerca processo figlio B\n");
+            for (int i = 6000; i < 10000; i++)
+            {
+                if (n == arrayRandom[i])
+                {
+                    printf("PID: %d, %d : %d\n", getpid(), i, arrayRandom[i]);
+                }
+            }
         }
         else
         {
             // Padre
-            int arrayRandom[array_length];
-            FILE *numeri;
-            for (int i = 0; i < array_length; i++)
+            printf("Ricerca processo padre\n");
+            for (int i = 0; i < 2000; i++)
             {
-                arrayRandom[i] = rand() % 500 + 1; // Generatore casuale di numeri da 1 a 500
-                numeri = fopen("numeriRandom.txt", "w");
-                if (numeri == NULL)
+                if (n == arrayRandom[i])
                 {
-                    printf("Errore nell'apertura del file\n");
-                    exit(1);
+                    printf("PID: %d, %d : %d\n", getpid(), i, arrayRandom[i]);
                 }
-                fwrite(buffer, 1, i, numeri);
-                fputc(':', numeri);
-                fwrite(buffer, 1, arrayRandom[i], numeri);
-                fputc('\n', numeri);
             }
-            fclose(numeri);
         }
     }
 
